be0c454d07cc3b3034993bec62b0072400f57297
Pac4 <amanoskull42@gmail.com>
2023-08-01 17:31:34 +0100

    Momments before disaster

--- src/main.c
+++ src/main.c
@@ -6,11 +6,15 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 12:28:38 by paugonca          #+#    #+#             */
-/*   Updated: 2023/07/27 15:55:28 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:30:45 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdlib.h>
+
+pthread_mutex_t	mutex;
+int				mails = 0;
 
 static void	philo_rules_set(t_philo *philo, char **av)
 {
@@ -24,15 +28,27 @@ static void	philo_rules_set(t_philo *philo, char **av)
 		philo->hunger = -1;
 }
 
+static void	*routine()
+{
+	while (mails < 1000000)
+	{
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	t_philo			philo;
+	t_philo		philo;
+	pthread_t	th[4];
+	int			p;
 
 	if (ac != 5 && ac != 6)
 		print_err("invalid number of arguments");
 	check_args(av);
 	printf("Arguments are valid!\n");
 	philo_rules_set(&philo, av);
-	if (philo.num == 0)
-		print_err("one or more philophers needed.");
+	return (EXIT_SUCCESS);
 }
